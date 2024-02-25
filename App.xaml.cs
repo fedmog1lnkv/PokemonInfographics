c3cac1393ac81da5e16ae14364e4e0e634eaf328
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2024-02-26 01:23:37 +0800

    feat: Configure DI

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,11 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PokemonInfographics.Domain.Interactors;
+using PokemonInfographics.Domain.Repositories;
+using PokemonInfographics.Infrastructure;
+using PokemonInfographics.Presentation.ViewModels;
+using System;
 using System.Windows;
 
 namespace PokemonInfographics
@@ -9,6 +15,48 @@ namespace PokemonInfographics
     /// </summary>
     public partial class App : Application
     {
-    }
+        private readonly IHost host;
+        public static IServiceProvider ServiceProvider { get; private set; }
 
+        public App()
+        {
+            host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+                    {
+                        ConfigureServices(services);
+                    })
+                    .Build();
+
+            ServiceProvider = host.Services;
+            Console.WriteLine("Service provider created");
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            await host.StartAsync();
+            var window = ServiceProvider.GetRequiredService<MainWindow>();
+            window.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // Регистрация репозитория и других зависимостей
+            services.AddSingleton<IPokemonRepository>(_ => CsvPokemonRepository.Instance);
+            services.AddScoped<MainWindow>();
+
+            // Регистрация ViewModel и интерактора с указанием их зависимостей
+            services.AddScoped<MainWindowViewModel>();
+            services.AddTransient<PokemonStatisticsInteractor>();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using (host)
+            {
+                await host.StopAsync(TimeSpan.FromSeconds(5));
+            }
+
+            base.OnExit(e);
+        }
+    }
 }
---
+++ Presentation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using GalaSoft.MvvmLight;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using PokemonInfographics.Domain.Interactors;
+using System;
+using System.Collections.Generic;
+
+namespace PokemonInfographics.Presentation.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        PokemonStatisticsInteractor _pokemonStatisticsInteractor;
+
+
+        public List<ISeries<double>> Series1 { get; set; }
+
+        public MainWindowViewModel(PokemonStatisticsInteractor pokemonStatisticsInteractor)
+        {
+            _pokemonStatisticsInteractor = pokemonStatisticsInteractor;
+
+
+            Console.WriteLine("ViewModel created");
+            var typeStatistics = _pokemonStatisticsInteractor.GetPokemonTypeStatistics();
+            Console.WriteLine(1);
+
+            var series = new List<ISeries<double>>();
+            foreach (var po in typeStatistics)
+            {
+                series.Add(new PieSeries<double> {Name = po.Key, Values = new double[] { po.Value } });
+            }
+            Series1 = series;
+        }
+    }
+}
